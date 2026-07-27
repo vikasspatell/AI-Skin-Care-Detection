a3620a15b58272c22b1791bdd60a6a31b13c17e5
vikasspatell <vikaspatel15062007@gmail.com>
2026-07-27 20:13:41 +0530

    Initial commit

---
+++ free_text_to_speech.py
@@ -0,0 +1,30 @@
+import subprocess
+import platform
+from gtts import gTTS
+
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+    os_name = platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            subprocess.run(['powershell', '-c', f'(New-Object Media.SoundPlayer "{output_filepath}").PlaySync();'])
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['aplay', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+input_text = "Hello, this is a test of the text-to-speech functionality. My name is AI with Hassan"
+output_filepath = "output_gtts.mp3"
+text_to_speech_with_gtts(input_text, output_filepath)
